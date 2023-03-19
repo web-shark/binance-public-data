631c819d267b94a687fa04540ffae404b989ab6a
Sergey Bliacharskiy <sergey16032000@gmail.com>
2023-03-19 20:03:38 +0200

    add files

--- python/to_one_file.py
+++ python/to_one_file.py
@@ -5,11 +5,11 @@ import numpy as np
 import pandas as pd
 
 paths = "data/spot/monthly/klines/BTCUSDT/5m/", "data/spot/monthly/klines/ETHUSDT/5m/"
+
+
 for path in paths:
     dirs = os.listdir(path)
     print(dirs)
-    # exit()
-
     all_data_array = []
     for file in dirs:
         print(path + file)
@@ -22,12 +22,10 @@ for path in paths:
     result_data = pd.concat(all_data_array)
     print(result_data)
     result_data.to_csv(path + 'sum.csv', header=False, index=False, sep=';')
-    train, test = \
-                  np.split(result_data.sample(frac=1, random_state=42),
-                           [int(.7*len(result_data))])
+    train, test = np.split(result_data,
+                  [int(.675 * len(result_data))])
     print(train)
     print(test)
     train.to_csv(path + 'train.csv', header=False, index=False, sep=';')
     test.to_csv(path + 'test.csv', header=False, index=False, sep=';')
-
 
